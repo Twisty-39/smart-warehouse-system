f98a787fd8da21450843b8fdb4faec55b66fe8a9
Twisty-39 <zaee.nzxt@gmail.com>
2026-09-18 21:32:27 +0700

    fix(backend): bind Kestrel to 0.0.0.0:5000 for Docker network upstream access

--- backend/SmartWarehouse.Api/Program.cs
+++ backend/SmartWarehouse.Api/Program.cs
@@ -11,7 +11,7 @@ using SmartWarehouse.Api.Services;
 using SmartWarehouse.Api.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://localhost:5000");
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
 // 1. ADD DATABASE CONTEXT (SQL Server)
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
